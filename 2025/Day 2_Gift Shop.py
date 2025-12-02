9580700a63e8968af689869b6bedf23f84a4fe48
Phak Chairatanakarn <phak.chairatanakarn@agoda.com>
2025-12-02 12:55:38 +0700

    update

--- 2025/Day 2_Gift Shop.py
+++ 2025/Day 2_Gift Shop.py
@@ -2,11 +2,10 @@
 
 def is_invalid_num(num):
     num_str = str(num)
-    digits = len(num_str)
-    mid = int(digits / 2)
+    mid = int(len(num_str) / 2)
 
     # number is invalid when first half and second half are the same
-    return digits % 2 == 0 and num_str[:mid] == num_str[mid:]
+    return num_str[:mid] == num_str[mid:]
 
 def invalid_sum(input_ids):
     ranges = input_ids.split(',')
@@ -21,3 +20,4 @@ def invalid_sum(input_ids):
 
 
 print(invalid_sum("197-407,262128-339499,557930-573266,25-57,92856246-93001520,2-12,1919108745-1919268183,48414903-48538379,38342224-38444598,483824-534754,1056-1771,4603696-4688732,75712519-75792205,20124-44038,714164-782292,4429019-4570680,9648251-9913729,6812551522-6812585188,58-134,881574-897488,648613-673853,5261723647-5261785283,60035-128980,9944818-10047126,857821365-857927915,206885-246173,1922-9652,424942-446151,408-1000"))
+# 19128774598
